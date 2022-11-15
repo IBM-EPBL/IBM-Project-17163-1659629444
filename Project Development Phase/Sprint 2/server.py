31030c94488b8c6b943f7ca5a0f8d2684d625665
Bhavana-1108 <117197803+Bhavana-1108@users.noreply.github.com>
2022-11-15 19:16:46 +0530

    Add Sprint 2 by Bhavana

---
+++ Project Development Phase/Sprint 2/server.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from flask import Flask, request, render_template, jsonify
+import pickle
+from sklearn import preprocessing
+import smtplib
+# my_email = "bhavuuu7@gmail.com"
+# my_password = "isang_saram_123"
+# subject = "HAPPY BIRTHDAY!!!!"
+
+app = Flask(__name__)
+model = pickle.load(open(r'model3.pkl', 'rb'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#
+#     return render_template('register.html')
+
+# @app.route('/loan', methods=['GET', 'POST'])
+# def loan():
+#
+#     return render_template('loan.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    return render_template("predict.html")
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    gender = request.form['query1']
+    married = request.form['query2']
+    dependents = request.form['query3']
+    education = request.form['query4']
+    self_employed = request.form['query5']
+    applicant_income = request.form['query6']
+    coapplicant_income = request.form['query7']
+    loan_amount = request.form['query8']
+    loan_amt_term = request.form['query9']
+    credit_history = request.form['query10']
+    property_area = request.form['query11']
+
+    input_feature = [[gender, married, dependents, education, self_employed, applicant_income, coapplicant_income,
+                      loan_amount, loan_amt_term, credit_history, property_area]]
+
+    print(input_feature)
+    names = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome',
+             'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+    data = pd.DataFrame(input_feature, columns=names)
+
+    new_df = data.copy()
+    label_encoder = preprocessing.LabelEncoder()
+    label_encoding_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area']
+    for col in label_encoding_columns:
+        new_df[col] = label_encoder.fit_transform(new_df[col])
+
+    prediction = model.predict(new_df)
+    print(prediction)
+    prediction = int(prediction)
+
+
+    # name = request.form['first-name']
+    # lname = request.form['last-name']
+    # recipient = request.form['email']
+    # print(name)
+
+    # if prediction == 0:
+    #     bday_letter = f"{name} Congratulations! Your loan will be approved"
+    # else:
+    #     bday_letter = f"{lname} Congratulations! Your loan will be approved"
+    #
+    # with smtplib.SMTP(host="smtp.gmail.com") as connection:
+    #     connection.starttls()
+    #     connection.login(my_email, my_password)
+    #     connection.sendmail(from_addr=my_email, to_addrs=recipient,
+    #                         msg="Subject:" + subject + "\n\n" +bday_letter)
+
+
+    if prediction == 0:
+        return render_template("submit.html", result="Loan will not be approved")
+    else:
+        return render_template("submit.html", result="Loan will be approved")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
